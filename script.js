c9348af88ecf9c8e93c23da33ff0c07fde2f0b72
CloakedReaper27 <finalx_153@hotmail.com>
2023-09-20 17:36:00 +0300

    added Dark mode

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function rate(stars) {
-    document.getElementById('review').textContent = 'Review: ' + stars + ' stars';
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    toggleButton.addEventListener('click', () => {
+        console.log("Test")
+        body.classList.toggle('dark-mode');
+    });
+});
