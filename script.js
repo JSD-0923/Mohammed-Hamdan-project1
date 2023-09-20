62661cea3fbc9f95aa31fd7e3516b60e3d2c6707
CloakedReaper27 <finalx_153@hotmail.com>
2023-09-20 18:59:09 +0300

    Main page and footer done

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log("Test")
         body.classList.toggle('dark-mode');
     });
+
+    document.getElementById('favoriteButton').addEventListener('click', function() {
+        var footer = document.getElementById('favoriteFooter');
+        footer.classList.toggle('hidden');
+      });
+      
+    //   function addToFavorites(title, content) {
+    //     var card = document.createElement('div');
+    //     card.classList.add('card');
+    //     card.innerHTML = '<h3>' + title + '</h3>' + '<p>' + content + '</p>';
+      
+    //     var favoriteCards = document.getElementById('favoriteCards');
+    //     favoriteCards.appendChild(card);
+    //   }
+    
 });
